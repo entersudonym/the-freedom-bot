e51061da6475e051b3772ff2ed439066d9079c9d
entersudonym <noahtheheir@gmail.com>
2020-05-01 13:06:54 -0700

    Make List command use canonical source of information

--- src/handlers/misc/List.ts
+++ src/handlers/misc/List.ts
@@ -3,6 +3,7 @@ import AbstractHandler from '../abstract/AbstractHandler'
 import { User } from '../../entity/User'
 import { Command } from '../../entity/Command'
 import { Message } from 'discord.js'
+import { In } from 'typeorm'
 
 export default class ListHandler extends AbstractHandler {
     public constructor() {
@@ -12,15 +13,31 @@ export default class ListHandler extends AbstractHandler {
     // TODO: This really should be using `handlers`, as that's the canonical source of what we
     // support / don't support.
     protected async handler(user: User, _cmd: Command, msg: Message): Promise<any> {
-        const query = user.isAdmin ? { isAdmin: true } : {}
-        const availableCommands = await Command.find(query)
-        let reply = "here's a list of available commands.\n\n"
+        const supportedInvocations = Array.from(handlers.keys())
+        const adminQuery = user.isAdmin ? {} : { isAdmin: false }
+        const availableCommands = await Command.find({
+            invocation: In(supportedInvocations),
+            ...adminQuery
+        })
 
+        let sections = {
+            daily: '**Daily Disciplines**\n',
+            misc: '**Miscellaneous Commands**\n',
+            admin: user.isAdmin ? '**Admin Commands**\n' : ''
+        }
         for (let availableCommand of availableCommands) {
-            reply += `**${availableCommand.name}**: !${availableCommand.invocation}\n`
-            reply += `${availableCommand.description}\n\n`
+            let row = `__${availableCommand.name}: !${availableCommand.invocation}__\n${availableCommand.description}\n\n`
+            if (availableCommand.isDaily) sections.daily += row
+            else if (availableCommand.isAdmin) sections.admin = row
+            else sections.misc += row
         }
 
-        return msg.reply(reply)
+        // Split so that we don't go over the 2000 character limit
+        let firstPart = `Here's a list of available commands.\n\n`
+        firstPart += sections.daily
+        let secondPart = sections.misc + sections.admin
+
+        msg.channel.send(firstPart)
+        return msg.channel.send(secondPart)
     }
 }
