648c87b49016f9b48ee8c0d860a119675dc9d785
entersudonym <noahtheheir@gmail.com>
2020-05-01 00:45:29 -0700

    Fix inverted list command

--- src/handlers/misc/List.ts
+++ src/handlers/misc/List.ts
@@ -12,7 +12,7 @@ export default class ListHandler extends AbstractHandler {
     // TODO: This really should be using `handlers`, as that's the canonical source of what we
     // support / don't support.
     protected async handler(user: User, _cmd: Command, msg: Message): Promise<any> {
-        const query = user.isAdmin ? {} : { isAdmin: true }
+        const query = user.isAdmin ? { isAdmin: true } : {}
         const availableCommands = await Command.find(query)
         let reply = "here's a list of available commands.\n\n"
 
