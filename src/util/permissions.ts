cf867520f363fb7dfc101e591e256cd30c4c8190
entersudonym <noahtheheir@gmail.com>
2020-06-27 14:12:10 -0700

    dont use build dir

--- src/util/permissions.ts
+++ src/util/permissions.ts
@@ -1,6 +1,6 @@
-import { hasRole } from '../../build/util/discord'
+import { hasRole } from '../util/discord'
 import { GuildMember } from 'discord.js'
-import { MiscServerRoles } from '../../build/data/roles'
+import { MiscServerRoles } from '../data/roles'
 
 export function isAdmin(user: GuildMember): boolean {
     return hasRole(user, [
