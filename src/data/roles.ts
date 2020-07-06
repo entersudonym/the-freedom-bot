04253294ad9baf82dced1bbf3e8df15e4a6219b6
entersudonym <noahtheheir@gmail.com>
2020-07-05 19:42:00 -0700

    update server roles

--- src/data/roles.ts
+++ src/data/roles.ts
@@ -1,7 +1,15 @@
+// THE FOLLOWING ARE FOR THE BETA CHANNEL
+// export enum MiscServerRoles {
+//     // TODO: Add the other roles and channels to a smiliar file; NOT config
+//     Executive = '705936078938570867',
+//     Moderator = '705936078938570866',
+//     Owner = '705936078938570868',
+//     TechGuy = '705936078938570865'
+// }
+
 export enum MiscServerRoles {
-    // TODO: Add the other roles and channels to a smiliar file; NOT config
-    Executive = '705936078938570867',
-    Moderator = '705936078938570866',
-    Owner = '705936078938570868',
-    TechGuy = '705936078938570865'
+    Executive = '540758777608142858',
+    Moderator = '624134002290196481',
+    Owner = '540757757926834177',
+    TechGuy = '550918127995584512',
 }
