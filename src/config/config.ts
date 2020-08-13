ac3a68600ff3400b7be7a0a33c67ea3c000e24c6
CastelShal <shalcashorrid@gmail.com>
2020-08-13 12:54:16 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,21 @@
 export default {
-    key: 'contact-entersudonym-for-this',
-    people: {
-        owner: '540386942131503115', // Probably Jakk
-        techLead: '390752251532279828' // Probably entersudonym
+    key: 'your_custom_key_here',
+    people: { // idt these are used
+        owner: '540386942131503115',
+        techLead: '390752251532279828'
     },
-    channels: {
-        mainChat: '647951827626622977',
-        newComers: '647941333784461366',
-        progressReporting: '647952101363679243',
-        adminChannel : '624134890647977995'
-
+    channels: { 
+        newComers: '542197462635380736',
+        rules: '542198344231944192',
+        mainChat: '545506091388108831',
+        nf: '541880649624059904',
+        progressReporting: '543115614953406474',
+        adminChannel: '624134890647977995',
+        botTalk: '548719339914919946'
+    },
+    urls: {
+        emergency: 'https://emergency.nofap.com/director.php?cat=em',
+        regression: 'https://emergency.nofap.com/director.php?cat=rel'
     }
 }
+
