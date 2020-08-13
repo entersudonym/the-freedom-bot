0ade2f80698bdc5a3b3a439af88d3c8126a238e4
CastelShal <shalcashorrid@gmail.com>
2020-08-13 12:27:45 +0530

    Create config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+export default {
+    key: 'contact-entersudonym-for-this',
+    people: {
+        owner: 'server-owner-here', // Probably Jakk
+        techLead: 'tech-leader-here' // Probably entersudonym
+    },
+    channels: {
+        mainChat: '647951827626622977',
+        newComers: '647941333784461366',
+        progressReporting: '647952101363679243',
+        adminChannel : '624134890647977995'
+
+    }
+}
