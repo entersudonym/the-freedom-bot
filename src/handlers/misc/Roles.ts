fe2e40f0dd0ac780f107c503dc76413f6593f555
CastelShal <shalcashorrid@gmail.com>
2021-02-08 19:19:44 +0530

    Rank checking for the command
    Checks if the user is on Janissary or above when assigning the role and removes the role on demotion below Janissary

--- src/handlers/misc/Roles.ts
+++ src/handlers/misc/Roles.ts
@@ -4,6 +4,7 @@ import AbstractHandler from '../abstract/AbstractHandler'
 import { Command } from '../../entity/Command'
 import { buildEmbed } from "../../util/embeds"
 import config from '../../config/config'
+import ranks from '../../data/ranks'
 
 export default class RoleHandler extends AbstractHandler {
     public constructor() {
@@ -13,7 +14,16 @@ export default class RoleHandler extends AbstractHandler {
     protected async handler(user: User, cmd: Command, msg: Message): Promise<any> {
         const userRoles: GuildMemberRoleManager = await msg.member.roles
         const raconteur = config.roles.raconteur
+        const janissaryLowerbound = ranks[4].lowerBound
         let roleAssigned = false
+
+        if(await user.points < janissaryLowerbound){    //Checks if the user is on Janissary or above
+            const embed : MessageEmbed = buildEmbed('E', 
+            "Failed to assign role",
+            msg.author.id,
+            "You must be on Janissary rank or above in order to get this role.")
+            return msg.channel.send(embed)
+        }
 
         if (await userRoles.cache.has(raconteur)) {
             await userRoles.remove(raconteur)
